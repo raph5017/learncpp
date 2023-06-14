cee6251df4666b462b72ea240232ec84d6ea92ef
rm <raph5019@gmail.com>
2023-06-14 15:57:56 +0900

    cleaned up directory

---
+++ chap1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Enter an integer: ";
+
+	int num{ }; // define variable num as an integer
+	std::cin >> num; // get integer value from user's keyboard
+
+	std::cout << "Enter another integer: ";
+
+	int num2{ }; // define variable num as an integer
+	std::cin >> num2; // get integer value from user's keyboard
+
+	std::cout << num << " + " << num2 << " is: " << num + num2 << '\n';
+
+	std::cout << num << " - " << num2 << " is: " << num - num2 << '\n';
+
+	return 0;
+
+}
+
