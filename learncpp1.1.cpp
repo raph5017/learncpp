8a190ca493925556cba479104a80ffa0665ffdf7
rm <raph5019@gmail.com>
2023-06-14 15:31:05 +0900

    changed filename

--- learncpp1.1.cpp
+++ learncpp1.1.cpp
@@ -7,8 +7,11 @@ int main()
 	int num{ }; // define variable num as an integer
 	std::cin >> num; // get integer value from user's keyboard
 
-	std::cout << "Double that number is: " << num * 2 << '\n'; // multiply number *2 when printing
+	std::cout << "Double " << num << " is: " << num * 2 << '\n'; // multiply number *2 when printing
+
+	std::cout << "Triple " << num << " is: " << num * 3 << '\n'; // multiply number *3 when printing
 
 	return 0;
+
 }
 
