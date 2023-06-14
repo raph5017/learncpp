bd32352dd85db9633997d63b89c8d995f1adacec
rm <raph5019@gmail.com>
2023-06-14 15:28:35 +0900

    added triple the number

--- learncpp1.1.cpp
+++ learncpp1.1.cpp
@@ -7,9 +7,7 @@ int main()
 	int num{ }; // define variable num as an integer
 	std::cin >> num; // get integer value from user's keyboard
 
-	std::cout << "Double " << num << " is: " << num * 2 << '\n'; // multiply number *2 when printing
-
-	std::cout << "Triple " << num << " is: " << num * 3 << '\n'; // multiply number *3 when printing
+	std::cout << "Double that number is: " << num * 2 << '\n'; // multiply number *2 when printing
 
 	return 0;
 }
