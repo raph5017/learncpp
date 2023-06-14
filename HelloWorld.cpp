1e4308c5dac52689ccccd25a9ae55986a1a28d75
rm <raph5019@gmail.com>
2023-06-14 15:22:40 +0900

    created a program that doubles the number you enter

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
 
-void doNothing(int&) // Don't worry about what & is for now, we're just using it to trick the compiler into thinking variable x is used
-{
-}
-
 int main()
 {
-    // define an integer variable named x
-    int x; // this variable is uninitialized
+	std::cout << "Enter an integer: ";
 
-    doNothing(x); // make the compiler think we're assigning a value to this variable
+	int num{ }; // define variable num as an integer
+	std::cin >> num; // get integer value from user's keyboard
 
-    // print the value of x to the screen (who knows what we'll get, because x is uninitialized)
-    std::cout << x << '\n';
+	std::cout << "Double that number is: " << num * 2 << '\n'; // multiply number *2 when printing
 
-    return 0;
+	return 0;
 }
+
