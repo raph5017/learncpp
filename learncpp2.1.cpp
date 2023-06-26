f1b494c301238c84d6a5a59b3a0fd4150b272349
rm <raph5019@gmail.com>
2023-06-26 16:29:50 +0900

    function parameters and arguments

--- learncpp2.1.cpp
+++ learncpp2.1.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 
-int getValueFromUser()
+int add(int x, int y)
 {
-	std::cout << "Enter an integer: ";
-	int input{};
-	std::cin >> input;
-
-	return input;
+    return x + y;
 }
 
-void printDouble(int value) // This function now has an integer parameter
+int multiply(int z, int w)
 {
-	std::cout << value << " doubled is: " << value * 2 << '\n';
+    return z * w;
 }
 
 int main()
 {
-	int num{ getValueFromUser() };
+    std::cout << add(4, 5) << '\n'; // within add() x=4, y=5, so x+y=9
+    std::cout << add(1 + 2, 3 * 4) << '\n'; // within add() x=3, y=12, so x+y=15
 
-	printDouble(num);
+    int a{ 5 };
+    std::cout << add(a, a) << '\n'; // evaluates (5 + 5)
 
-	return 0;
+    std::cout << add(1, multiply(2, 3)) << '\n'; // evaluates 1 + (2 * 3)
+    std::cout << add(1, add(2, 3)) << '\n'; // evaluates 1 + (2 + 3)
+
+    return 0;
 }
